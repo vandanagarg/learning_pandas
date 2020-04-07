8d97b7d27d9c487c49467a73cf598c44b2dacf76
vandana garg <er.vandanagarg@gmail.com>
2020-04-07 15:33:27 +0200

    added 3types of package/module import using __init__.py file

--- pandas_udemy_bootcamp/src/pandas_series/analyze.py
+++ pandas_udemy_bootcamp/src/pandas_series/analyze.py
@@ -1,9 +1,9 @@
 """ Analyze single columns of a DataFrame (Pandas Series) """
 import config_file  # noqa: F401
-# from utilities.dataframe_utilities import DataframeUtilities as dfu
-from utilities import DataframeUtilities as dfu
+from utilities import DataframeUtilities as dfu  # Type 1, 2
+# from utilities.dataframe_utilities import DataframeUtilities as dfu  # Type 3
 
-summer_df = dfu.get_dataframe("summer.csv")
+summer_df = dfu.get_dataframe("summer.csv")  # Type 1, 2, 3
 print(summer_df)
 
 summer = dfu.get_indexed_dataframe("summer.csv", "Athlete")
