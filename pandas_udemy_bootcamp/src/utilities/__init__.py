67132f330bc987e4576a7e0ecf32033e2c2fae7c
vandana garg <er.vandanagarg@gmail.com>
2020-05-07 18:17:46 +0200

    added module to create dataframe for Course_Materials_Part2

--- pandas_udemy_bootcamp/src/utilities/__init__.py
+++ pandas_udemy_bootcamp/src/utilities/__init__.py
@@ -1,3 +1,5 @@
+''' to facilitate easy import of classes '''
 from utilities.dataframe_utilities import DataframeUtilities  # noqa: F401
+from utilities.dataframe_utilities_project import DataframeUtilitiesProject  # noqa: F401, E501
 # from utilities.dataframe_utilities import *  # noqa: F401
 # from utilities import dataframe_utilities  # noqa: F401
