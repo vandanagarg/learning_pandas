558aa045a9c22801671dffa6cf4f2e6b473fc2d2
vandana garg <er.vandanagarg@gmail.com>
2020-09-04 12:53:27 +0200

    added code for Time Series - Covariance and Correlation

--- pandas_udemy_bootcamp/src/time_series_data/return_risk.py
+++ pandas_udemy_bootcamp/src/time_series_data/return_risk.py
@@ -1,6 +1,7 @@
 ''' Financial Time Series - Return and Risk '''
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from create_dataframe import close
 
 
@@ -26,6 +27,7 @@ print(summary)
 # annual mean and std/ returns or risks
 summary["mean"] = summary["mean"]*252
 summary["std"] = summary["std"] * np.sqrt(252)
+print("\n Summary df:")
 print(summary)
 
 # graphical plot - scatter plot expected 6 dots
@@ -39,4 +41,30 @@ for i in summary.index:
 plt.xlabel("ann. Risk(std)", fontsize=15)
 plt.ylabel("ann. Return", fontsize=15)
 plt.title("Risk/Return", fontsize=20)
+plt.show()
+
+''' Financial Time Series - Covariance and Correlation
+For portfolio management of stocks:
+Just along with analysing returns and risk we must observe
+how two stocks perform and move along if one is rising is
+other rising too or vice versa '''
+print(ret.head())
+
+# covariance matrix is used to calculate risk matrix
+print("\n Covariance df:")
+print(ret.cov())
+
+# To check if two stocks move along correlation is more appropriate matrix
+# value -1 to +1
+# +1 means stocks move perfectly along
+# -1 means stocks move complete opposite
+print("\n Correlation df:")
+print(ret.corr())
+
+# visual heat map for correlation matrix
+plt.figure(figsize=(12, 8))
+sns.set(font_scale=1.4)
+# set upper scale vmax=0.6
+sns.heatmap(ret.corr(), cmap="Reds", annot=True,
+            annot_kws={"size": 15}, vmax=0.6)
 plt.show()
